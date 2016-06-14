9c93d4e7d1208f1dbffb96804bf6dbb88ce02f7f
Sebastian Waschnick <sebastian.waschnick@asideas.de>
2016-06-14 16:46:18 +0200

    Tiles Initial commit

--- src/js/views/PreviousHackathons/index.js
+++ src/js/views/PreviousHackathons/index.js
@@ -1,10 +1,40 @@
 import React from 'react';
 
+var Masonry = require('react-masonry-component');
+
+
+const Tile = React.createClass({
+    render: function () {
+        return (
+            <div>
+                <div className="tile image-element-class">
+                    <div className="tileTitlePicture">{this.title}</div>
+                    <img className="image" src={ require(this.imageSrc) }/>
+                </div>
+            </div>
+        );
+    }
+});
+
 class PreviousHackathons extends React.Component {
+    const masonryOptions = {
+        transitionDuration: 0
+    };
+
     render() {
         return (
             <div className="previous-hackathons content-container">
-                TODO
+                <Masonry
+                    className={'my-gallery-class'} // default ''
+                    elementType={'ul'} // default 'div'
+                    options={ this.masonryOptions } // default {}
+                    disableImagesLoaded={false} // default false
+                >
+                    <Tile imageSrc="./images/Bitmap01.jpg"/>
+                    <Tile imageSrc="./images/Bitmap02.jpg"/>
+                    <Tile imageSrc="./images/Bitmap03.jpg"/>
+                </Masonry>
+
             </div>
         );
     }
