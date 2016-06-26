01f1103772dcdc5f7a82639c4c09564cb34aa530
unknown <Sebastian@Waschnick.de>
2016-06-26 16:01:34 +0200

    GetYouTicket Waitlist styling

--- src/js/views/GetYourTicket/index.js
+++ src/js/views/GetYourTicket/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+require('./main.styl');
 // id=611d9dd825b3f04ff5d61928ed952399&uuid=611d9dd825b3f04ff5d61928ed952399&email=barriwaschi+1@gmail.com&affiliate=6d855ed168eb&name=&referred_count=0&position=1&activated_at=&created_at=2016-06-25T20:37:20.420Z&referred_by=
 
 
@@ -19,11 +20,15 @@ class GetYourTicket extends React.Component {
         } else {
             return (
                 <div className="get-your-ticket content-container">
-                    Blablabla ticket
+                    <h1>You want to participate<span className="other-font">?</span></h1>
+
+                    <h2>Stay tuned and sign up!</h2>
+
+                    <br />
 
                     <form action="https://mobilityhacks.app.waitlisted.co/external/reservations" method="POST">
-                        <label>Email</label>
-                        <input name="reservation[email]"/>
+                        <input className="email-input" name="reservation[email]" placeholder="Your E-Mail"/>
+                        <br />
                         <input type="submit" value="Sign Up"/>
                     </form>
                 </div>
