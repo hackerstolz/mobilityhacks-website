e2c6de89fc6879d971ab571218e91c4f64d30045
Nico Ring <ringnico@web.de>
2016-10-01 10:46:28 +0200

    adds schedule events

--- src/js/views/Station-06-Schedule/index.js
+++ src/js/views/Station-06-Schedule/index.js
@@ -4,7 +4,30 @@ import Modal from '../../layout/Modal/Modal';
 const titletext = 'Friday get together - Saturday Code Start<br />' +
     'Sunday Presentations & Celebrations';
 
+
+const fridayEvents = [
+  'Get Together 7:30pm'
+];
+
+const saturdayEvents = [
+  'Registration starts at 8:30am',
+  'Kick-off and challenge pitches 10am',
+  'Workshops take place from 11am to 12am ',
+  'Hacking starts at 12pm'
+];
+
+const sundayEvents = [
+  'End of hacking at 12pm',
+  'Your pitches will between 13pm and 15pm',
+  'Price ceremony at 16pm'
+];
+
 class ModalView extends React.Component {
+
+    eventTags(eventNames) {
+      return eventNames.map((name, i) => <p key={i}>{name}</p>)
+    }
+
     render() {
         return (
             <div className="schedule content-container">
@@ -18,7 +41,7 @@ class ModalView extends React.Component {
                         <legend>Friday Dec 2</legend>
                         <div className="jury__fieldset__container">
                             <div className="jury__fieldset__container-wrapper">
-                                <p>Get Together 8pm</p>
+                                {this.eventTags(fridayEvents)}
                             </div>
                         </div>
                     </fieldset>
@@ -27,7 +50,7 @@ class ModalView extends React.Component {
                         <legend>Saturday Dec 3</legend>
                         <div className="jury__fieldset__container">
                             <div className="jury__fieldset__container-wrapper">
-                                <p>Kickstart at 11 am</p>
+                                {this.eventTags(saturdayEvents)}
                             </div>
                         </div>
                     </fieldset>
@@ -36,14 +59,10 @@ class ModalView extends React.Component {
                         <legend>Sunday Dec 4</legend>
                         <div className="jury__fieldset__container">
                             <div className="jury__fieldset__container-wrapper">
-                                <p>Stop 12pm</p>
-                                <p>Presentation 2pm</p>
-                                <p>Ceremony 3:30pm</p>
+                                {this.eventTags(sundayEvents)}
                             </div>
                         </div>
                     </fieldset>
-
-
                 </Modal>
             </div>
         );
