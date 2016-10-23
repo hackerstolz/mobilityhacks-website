22d4223aa990c2f71ad343970f64bd45206d388a
unknown <Sebastian@Waschnick.de>
2016-10-23 17:42:40 +0200

    WIP

--- src/js/views/Station-04-Organizers/index.js
+++ src/js/views/Station-04-Organizers/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import Modal from '../../layout/Modal/Modal';
 
-const titletext = 'Why us, I don\'t know. ll';
+const titletext = 'Why us, I don\'t know! We are all huge fans of Hackathons and want to share ' +
+    'the joy we had at other events. So we decided to host our own. This is our 2nd Hackathon in Berlin, ' +
+    'we are Hackerstolz and we hope you like our super cool events.<br /><br /><a href="https://youtu.be/R8myY9XjiN0" target="_blank">' +
+    'You can watch a movie about our last Hackathon.</a>';
 
 class ModalView extends React.Component {
     render() {
@@ -18,41 +21,41 @@ class ModalView extends React.Component {
                             <div className="fieldset__container-wrapper">
                                 <div className="circle-image__container--big">
 
-                                    <img className="circle-image" src={require('./iris_broese_bw.png')}/>
+                                    <img className="circle-image" src={require('./pictures/iris_broese_bw.png')}/>
                                     <p className="image-text-title">Iris Bröse</p>
                                     <p className="image-text">Sponsors, Planning, Interior, Catering</p>
                                 </div>
-                                 <div className="circle-image__container--big">
-                                    <img className="circle-image" src={require('./oezden_gelgec_bw.png')}/>
+                                <div className="circle-image__container--big">
+                                    <img className="circle-image" src={require('./pictures/oezden_gelgec_bw.png')}/>
                                     <p className="image-text-title">Özden Gelgeç</p>
                                     <p className="image-text">Identity, Interior Design</p>
                                 </div>
                                 <div className="circle-image__container--big">
-                                    <img className="circle-image" src={require('./alex_bw.png')}/>
+                                    <img className="circle-image" src={require('./pictures/alex_bw.png')}/>
                                     <p className="image-text-title">Alexander Müller</p>
                                     <p className="image-text">Finance, Moderation, Planning</p>
                                 </div>
                                 <div className="circle-image__container--big">
-                                    <img className="circle-image" src={require('./daniel_seiler_bw.png')}/>
+                                    <img className="circle-image" src={require('./pictures/daniel_seiler_bw.png')}/>
                                     <p className="image-text-title">Daniel Seiler</p>
                                     <p className="image-text">Identity</p>
                                 </div>
                                 <div className="circle-image__container--big">
-                                    <img className="circle-image" src={require('./frederik_chettouh_bw.png')}/>
+                                    <img className="circle-image" src={require('./pictures/frederik_chettouh_bw.png')}/>
                                     <p className="image-text-title">Frederik Chettouh</p>
                                     <p className="image-text">Sponsors, Service, Moderation</p>
                                 </div>
                                 <div className="circle-image__container--big">
-                                    <img className="circle-image" src={require('./nico_ring_bw.png')}/>
+                                    <img className="circle-image" src={require('./pictures/nico_ring_bw.png')}/>
                                     <p className="image-text-title">Nico Ring</p>
                                     <p className="image-text">Sponsors, Tech Partnerships, Infrastructure</p>
                                 </div>
                                 <div className="circle-image__container--big">
 
-                                    <img className="circle-image" src={require('./sebastian_waschnick_bw.png')}/>
+                                    <img className="circle-image" src={require('./pictures/sebastian_waschnick_bw.png')}/>
                                     <p className="image-text-title">Sebastian Waschnick</p>
                                     <p className="image-text">Tech Partnerships, Website</p>
-                                </div>                                
+                                </div>
                             </div>
                         </div>
                     </fieldset>
