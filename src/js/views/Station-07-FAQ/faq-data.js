badbfb2841b3352ef0fe93e6c2b585f9aa620749
unknown <Sebastian@Waschnick.de>
2016-11-03 09:10:47 +0100

    FAQ !!

--- src/js/views/Station-07-FAQ/faq-data.js
+++ src/js/views/Station-07-FAQ/faq-data.js
@@ -1,28 +1,58 @@
 let data = [
     {
         id: 1,
-        header: "Will there be food?",
-        text: "Yes we will have some awesome Catering for you guys! And the best thing about is, it will be for free for you!"
+        header: "What is a hackathon?",
+        text: "A hackathon is any event of any duration where people come together to solve problems with digital means and present them to each other. That’s it, really simple. In most cases a hackathon is on two days and you get 24 hours time for the hacking. Hacking is meant in the sense of exploratory programming, not referenced to any computer crime."
     },
     {
         id: 2,
+        header: "Do I need to pay for the tickets?",
+        text: "No. But: You will pay 15€ for the tickets on eventbrite, but you will get 15€ cashback when you show up."
+    },
+    {
+        id: 9,
+        header: "Are there travel cost reimbursements?",
+        text: "Yes. there is! Please submit the this <a href='https://goo.gl/forms/BUucIhhRkwFERYuV2' target='_blank'> form </a> and we will come back to you for details!"
+    },
+    {
+        id: 3,
+        header: "Will there be food?",
+        text: "Yes. We will have some awesome Catering for you guys! And the best thing about is, it will be for free for you! Did we mention the after show party with free beer?"
+    },
+    {
+        id: 4,
+        header: "What should I bring?",
+        text: "Tricky question. Grab all you need to work. Most likely your phone, charger, your laptop, charger and some headphones. Bringing toiletries is also a good idea - take your toothpaste, towel, deodorant etc. and extra clothes. If you enjoy napping, don’t forget a throw pillow and a sleeping bag. We will have drinks and food for free at the location, don’t worry. You will also be provided with free Internet access."
+    },
+    {
+        id: 5,
+        header: "What if I've never been to a hackathon?",
+        text: "Don’t worry. Mobility{hacks} is specifically designed to accommodate beginners as well as people with more experience. If you've never been to a hackathon, or are just getting started coding: Don't fear! We will be holding info sessions, team-finding events, and will help you in person if necessary. It will be a safe environment. There will also be workshops during the hackathon, like a pitch and presentation training from one our partners."
+    },
+    {
+        id: 6,
         header: "Where can I sleep?",
         text: "We will have some areas where you can nap for a couple of hours. However, if you plan to have a relaxing good nights sleep, you should book a Bed in a Hostel."
     },
     {
-        id: 3,
+        id: 7,
         header: "How big are the teams?",
-        text: "We will allow team of the size of 2-5 People."
+        text: "Hackathons are more fun when you work with others, so we encourage people to form teams beforehand or find teams at the event. We will allow team of the size of 2-5 People."
     },
     {
-        id: 4,
+        id: 8,
         header: "Who is Hackerstolz?",
         text: "One organisation to rule them all, no no just kidding. We are a non-profit that wants to contribute to an active IT community in Germany. You can find more info on hackerstolz.de"
     },
     {
-        id: 5,
-        header: "Are there travel cost reimbursements?",
-        text: "Maybe, we'll let you know asap."
+        id: 10,
+        header: "Can I code something before the hackathon?",
+        text: "No. As we want to keep it as fair as possible everyone gets the same 24 hours to work on their projects. We will check the source code"
+    },
+    {
+        id: 11,
+        header: "Where can I find the full rules?",
+        text: "Check out our page on <a href='https://mobility-hacks.devpost.com/' target='_blank'> devpost</a>, we will have the rules their on time."
     },
 ];
 
