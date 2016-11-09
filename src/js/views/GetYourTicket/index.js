0ef442e5d05d215c68c63e01e00102fd2dc87f87
Daniel Seiler <ds@nwebs.de>
2016-11-09 15:40:24 +0100

    daniel design stuff

--- src/js/views/GetYourTicket/index.js
+++ src/js/views/GetYourTicket/index.js
@@ -6,9 +6,11 @@ class GetYourTicket extends React.Component {
     render() {
         return (
             <div className="content-container get-your-ticket">
-                <h1>Get your ticket now!</h1>
-                <br />
-                <p>Talk to evenbrite at the counter. Please note that you get 15€ cashback at the event!</p>
+                <div className="textbox">
+                    <h1>Get your ticket now!</h1>
+                    <br />
+                    <p>Talk to evenbrite at the counter. Please note that you get 15€ cashback at the event!</p>
+                </div>
 
                 <a href="//www.eventbrite.de/e/mobilityhacks-hackathon-about-the-future-of-mobility-tickets-27795158066" target="_blank">
                     <div className="small-button">Go to Eventbrite</div>
