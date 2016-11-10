63a8a3810503583582494d0b7d385a52916d8f36
sebastian.waschnick <sebastian@waschnick.de>
2016-11-10 22:32:28 +0100

    Styling

--- src/js/views/Station-08-Footer/index.js
+++ src/js/views/Station-08-Footer/index.js
@@ -4,6 +4,12 @@ class ModalView extends React.Component {
     render() {
         return (
             <div id="footer" className="footer wide content-container">
+                <div className="footer__row">
+                    <div className="footer__column"></div>
+                    <div className="footer__column"></div>
+                    <div className="footer__column"></div>
+                    <div className="footer__column"></div>
+                </div>
             </div>
         );
     }
