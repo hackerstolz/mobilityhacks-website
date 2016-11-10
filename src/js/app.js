f59bb4d57d40734cfd97f4cc11590fcb289846d2
Sebastian Waschnick <sebastian.waschnick@asideas.de>
2016-11-10 12:48:00 +0100

    Fixing build

--- src/js/app.js
+++ src/js/app.js
@@ -17,32 +17,21 @@ require('./../fonts/fonts.styl');
 // Views (if you have an index.js you can import folders)
 import Layout from './layout/Layout';
 import Home from './views/Home/';
-import Home2 from './views/Home2/';
-import BecomeASponsor from './views/BecomeASponsor/';
-import GetYourTicket from './views/GetYourTicket/';
-import PreviousHackathons from './views/PreviousHackathons/';
-import Station01 from './views/Station-01-Mobility/';
-import Station02 from './views/Station-02-Prizes-and-Jury';
-import Station03 from './views/Station-03-Jury';
-import Station04 from './views/Station-04-Organizers';
-import Station05 from './views/Station-05-Sponsors';
-import Station06 from './views/Station-06-Schedule';
-import Station07 from './views/Station-07-FAQ';
 
 ReactDom.render(
     <Router history={ browserHistory }>
         <Route name="root" path="/" component={ Layout }>
-            <IndexRoute name="home" component={ Home2 }/>
-            <Route name="home" path="home(#**)" component={ Home2 }/>
-            <Redirect from="get-your-ticket" to="home#get-your-ticket" />
-            <Redirect from="contact" to="home#organizers" />
-            <Redirect from="station-01-why-mobility" to="home#why-mobility" />
-            <Redirect from="station-02-prizes-and-criteria" to="home#prizes" />
-            <Redirect from="station-03-jury" to="home#jury" />
-            <Redirect from="station-04-organizers" to="home#organizers" />
-            <Redirect from="station-05-sponsors" to="home#sponsors" />
-            <Redirect from="station-06-schedule" to="home#schedule" />
-            <Redirect from="station-07-faq" to="home#faq" />
+            <IndexRoute name="home" component={ Home }/>
+            <Route name="home" path="home(#**)" component={ Home }/>
+            <Redirect from="get-your-ticket" to="home#get-your-ticket"/>
+            <Redirect from="contact" to="home#organizers"/>
+            <Redirect from="station-01-why-mobility" to="home#why-mobility"/>
+            <Redirect from="station-02-prizes-and-criteria" to="home#prizes"/>
+            <Redirect from="station-03-jury" to="home#jury"/>
+            <Redirect from="station-04-organizers" to="home#organizers"/>
+            <Redirect from="station-05-sponsors" to="home#sponsors"/>
+            <Redirect from="station-06-schedule" to="home#schedule"/>
+            <Redirect from="station-07-faq" to="home#faq"/>
         </Route>
     </Router>,
     document.getElementById('app')
---
+++ src/js/views/Station-00-Start/index.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { Link } from 'react-scroll';
+require('./main.styl');
+
+class Home extends React.Component {
+    render() {
+        return (
+            <div id="home" className="content-container home">
+                <div  className="startseite-logo__container">
+                        <img className="startseite-logo__img_br" src={require('./img/splash_icon_background.svg')}/>
+                        <div className="startseite-logo__img_wrap">
+                            <img className="startseite-logo__img" src={require('./img/mobility_logo.svg')}/>
+                        </div>
+                </div>
+
+                <div className="home__teaser-text">
+                    <ul>
+                        <li>
+                            date:<br />
+                            saturday, 03/12/16<br />
+                            sunday, 04/12/16
+                        </li>
+                        <li>
+                            location:<br />
+                            spielfeld digital hub<br />
+                            skalitzer str. 85/86<br />
+                            10993 berlin
+                        </li>
+
+                        <li className="hidden">
+                            <Link to="get-your-ticket">tickets</Link>:<br />
+                        </li>
+                        <li className="hidden">
+                            this hackathon is<br />
+                            organized by <a href="http://www.hackerstolz.de" target="_blank">hackerstolz</a><br />
+                            <div className="home__social-icons">
+                                <a href="https://www.facebook.com/events/842521752516067/" target="_blank">
+                                    <img className="social-icon--facebook" src={require('./social-icons/facebook.svg')}/>
+                                </a>
+                                <a href="https://youtu.be/R8myY9XjiN0" target="_blank">
+                                    <img className="social-icon--youtube" src={require('./social-icons/youtube.svg')}/>
+                                </a>
+                                <a href="https://github.com/hackerstolz" target="_blank">
+                                    <img className="social-icon--github" src={require('./social-icons/github.svg')}/>
+                                </a>
+                                <a href="https://twitter.com/hackerstolz" target="_blank">
+                                    <img className="social-icon--twitter" src={require('./social-icons/twitter.svg')}/>
+                                </a>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Home;
